62cdc487c4dd8985034ebff16767b81012657f40
Jeff West <jeffrey.west@oracle.com>
2013-06-06 11:16:00 -0700

    reorganized websockets examples

--- WInS_Demos/initialize.sh
+++ WInS_Demos/initialize.sh
@@ -10,6 +10,6 @@ if [ "$?" == "0" ]; then
   mvn -DskipTests=true install
 fi
 
-mvn install:install-file -Dfile=/u01/wls1212/wlserver/modules/com.bea.core.weblogic.web.api_3.0.0.0.jar -DgroupId=com.bea.core.weblogic.web -DartifactId=api -Dversion=12.1.2.0 -Dpackaging=jar
+cd
 
 echo "Initialization complete!!!"
