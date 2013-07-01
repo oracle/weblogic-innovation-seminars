6d32c964081499303e35c0f6e0981eb982b7ec16
Jeff West <jeffrey.west@oracle.com>
2013-07-01 10:53:33 -0700

    updated for 12

--- WInS_Demos/control/bin/winsEnv.sh
+++ WInS_Demos/control/bin/winsEnv.sh
@@ -13,8 +13,8 @@ export USER_BASE="/u01"
 
 ##################################################################
 
-export ORACLE_HOME="${SW_BASE}/app/oracle/product/11.2.0/xe"
-export ORACLE_SID="XE"
+export ORACLE_HOME="${SW_BASE}/app/oracle/product/12.1/database"
+export ORACLE_SID="orcl"
 export NLS_LANG=`$ORACLE_HOME/bin/nls_lang.sh`
 export PATH="${ORACLE_HOME}/bin:${PATH}"
 
