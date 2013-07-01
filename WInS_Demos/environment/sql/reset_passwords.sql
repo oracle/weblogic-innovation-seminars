0ebc39d9ad83322c499c6bb2b0cd6356ab807a75
Jeff West <jeffrey.west@oracle.com>
2013-07-01 13:35:01 -0700

    updated for PDB

--- WInS_Demos/environment/sql/reset_passwords.sql
+++ WInS_Demos/environment/sql/reset_passwords.sql
@@ -1,11 +1,14 @@
+alter session set container=PDBORCL
+
 ALTER PROFILE DEFAULT LIMIT
 FAILED_LOGIN_ATTEMPTS UNLIMITED
 PASSWORD_LIFE_TIME UNLIMITED;
 
+
 alter user sys identified by welcome1;
 alter user system identified by welcome1;
 alter user ops_user identified by ops_user;
-alter user ops identified by ops;]
+alter user ops identified by ops;
 
 alter user weblogic_examples_domain identified by weblogic_examples_domain;
 alter user saf_target_domain identified by saf_target_domain;
