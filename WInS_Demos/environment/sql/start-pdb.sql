b29bde000a9e5f3ae85c4e6b8ebda51e677a688e
Jeff West <jeffrey.west@oracle.com>
2013-07-02 20:36:43 -0700

    added 'exit'

--- WInS_Demos/environment/sql/start-pdb.sql
+++ WInS_Demos/environment/sql/start-pdb.sql
@@ -1,3 +1,5 @@
 alter session set container=PDBORCL;
 alter pluggable database PDBORCL open;
 execute dbms_service.start_service('orcl_pdb');
+
+exit;
--- WInS_Demos/environment/sql/stop-pdb.sql
+++ WInS_Demos/environment/sql/stop-pdb.sql
@@ -1,3 +1,5 @@
 alter session set container=PDBORCL;
 execute dbms_service.stop_service('orcl_pdb');
 alter pluggable database PDBORCL close;
+
+exit;
