5f2b26ab1d874996042611fc352ba70d50420573
Jeff West <jeffrey.west@oracle.com>
2013-07-05 16:33:02 -0700

    fixed!  Added messages for all proxy settings

--- WInS_Demos/control/bin/updateDemos.sh
+++ WInS_Demos/control/bin/updateDemos.sh
@@ -8,9 +8,13 @@ export GIT_URL="http://github.com/oracle-weblogic/weblogic-innovation-seminars.g
 
 echo "Updating WInS Demos in ${CONTENT_DIR}..."
 
-GIT_PROXY=`git config --get http.proxy`
+GIT_SYSTEM_PROXY=`git config --get --system http.proxy`
+GIT_GLOBAL_PROXY=`git config --get --global http.proxy`
+GIT_PROJECT_PROXY=`git config --get -f $DEMOS_HOME/../.git/config http.proxy`
 
-echo "GIT Proxy set to: [${GIT_PROXY}]"
+echo "GIT _system_ Proxy set to: [${GIT_SYSTEM_PROXY}]"
+echo "GIT _global_ Proxy set to: [${GIT_GLOBAL_PROXY}]"
+echo "GIT _project_ Proxy set to: [${GIT_PROJECT_PROXY}]"
 
 cd ${CONTENT_DIR}
 
@@ -19,7 +23,7 @@ git fetch --tags
 if [ "$?" == "0" ]; then
   git merge ${TAG_NAME}
 else
-  echo "The update operation has failed.  Please check your proxy settings, especially if you are on the Oracle network.  The GIT Proxy is set to: [${GIT_PROXY}\]"
+  echo "The update operation has failed.  Please check your proxy settings, especially if you are on the Oracle network.  The GIT Proxy is set to: [${GIT_GLOBAL_PROXY}\]"
 fi
 
 if [ "$1" == "wait" ]; then
