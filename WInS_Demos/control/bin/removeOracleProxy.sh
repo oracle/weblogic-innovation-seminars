1873b31d862b7d409243b83cb2f4f2a178275727
Jeff West <jeffrey.west@oracle.com>
2013-07-05 16:21:04 -0700

    resovling "" issue

--- WInS_Demos/control/bin/removeOracleProxy.sh
+++ WInS_Demos/control/bin/removeOracleProxy.sh
@@ -4,14 +4,12 @@ PROXY_SCRIPT="/home/oracle/setProxy.sh"
 PROXY_MESSAGE="Proxy NOT SET for Oracle Network!!!"
 
 echo "#!/bin/sh" > ${PROXY_SCRIPT}
-echo "export http_proxy=\"\"" >> ${PROXY_SCRIPT}
-echo "sudo git config --global http.proxy \"\"" >> ${PROXY_SCRIPT}
 echo "echo \"${PROXY_MESSAGE}\"" >> ${PROXY_SCRIPT}
 echo "echo \"${PROXY_MESSAGE}\"" >> ${PROXY_SCRIPT}
 echo "echo \"${PROXY_MESSAGE}\"" >> ${PROXY_SCRIPT}
 
-sudo git config --global --unset http.proxy
-sudo git config --unset http.proxy
+sudo git config --global http.proxy ""
+sudo git config http.proxy ""
 export http_proxy=
 
 echo "Removing Proxy Settings from GIT!"
--- WInS_Demos/control/bin/setOracleProxy.sh
+++ WInS_Demos/control/bin/setOracleProxy.sh
@@ -5,8 +5,6 @@ ORACLE_HTTP_PROXY="http://www-proxy.us.oracle.com:80"
 PROXY_MESSAGE="Proxy Configured for Oracle Network!!!"
 
 echo "#!/bin/sh" > ${PROXY_SCRIPT}
-echo "export http_proxy=${ORACLE_HTTP_PROXY}" >> ${PROXY_SCRIPT}
-echo "sudo git config --global http.proxy ${ORACLE_HTTP_PROXY}" >> ${PROXY_SCRIPT}
 echo "echo \"${PROXY_MESSAGE}\"" >> ${PROXY_SCRIPT}
 echo "echo \"${PROXY_MESSAGE}\"" >> ${PROXY_SCRIPT}
 echo "echo \"${PROXY_MESSAGE}\"" >> ${PROXY_SCRIPT}
