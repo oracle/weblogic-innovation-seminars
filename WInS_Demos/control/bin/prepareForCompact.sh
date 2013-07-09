1560112f01ce2b7ff55a5e8353b87432e6a45f36
Jeff West <jeffrey.west@oracle.com>
2013-07-08 20:44:20 -0700

    fixed logic error... scripts cannot be run if you just deleted them...

--- WInS_Demos/control/bin/prepareForCompact.sh
+++ WInS_Demos/control/bin/prepareForCompact.sh
@@ -18,23 +18,10 @@ killWebLogic.sh
 sudo rm -Rf $DOMAINS
 sudo rm -Rf /tmp/*
 sudo rm -Rf /var/cache/yum/*
-sudo rm -Rf /u01/content/oracle-parcel-service/ops-weblogic/ops-embedded/Oracle
+sudo rm -Rf /u01/content/oracle-parcel-service/ops-weblogic/Oracle
 sudo rm -Rf /home/oracle/Downloads/*
 sudo rm -Rf /home/oracle/.m2/repository/com/oracle/weblogic/demo
 sudo rm -Rf /home/oracle/.m2/repository/com/oracle/demo
-
-# Check for symlinks on the content directories.  If they are links, delete them
-# they should be put back by the update*.sh scripts.
-if [ -h /u01/content/weblogic-innovation-seminars ]; then
-  rm /u01/content/weblogic-innovation-seminars
-fi
-
-if [ -h /u01/content/oracle-parcel-service ]; then
-  rm /u01/content/oracle-parcel-service
-fi
-
-/u01/content/weblogic-innovation-seminars/WInS_Demos/control/updateDemos.sh
-/u01/content/weblogic-innovation-seminars/WInS_Demos/control/updateOPS.sh
 
 cd /u01/content/weblogic-innovation-seminars/WInS_Demos
 mvn clean
@@ -46,6 +33,22 @@ find /u01/content/weblogic-innovation-seminars -name "*.sh" -exec chmod +rx {} \
 
 zerofree /tmp
 zerofree /u01
+
+if [ -h /u01/content/weblogic-innovation-seminars ]; then
+  rm /u01/content/weblogic-innovation-seminars
+  cd /u01/content
+    git clone http://github.com/oracle-weblogic/weblogic-innovation-seminars.git
+else
+  /u01/content/weblogic-innovation-seminars/WInS_Demos/control/updateDemos.sh
+fi
+
+if [ -h /u01/content/oracle-parcel-service ]; then
+  rm /u01/content/oracle-parcel-service
+  cd /u01/content
+  git clone http://github.com/jeffreyawest/oracle-parcel-service.git
+else
+  /u01/content/weblogic-innovation-seminars/WInS_Demos/control/updateOPS.sh
+fi
 
 
 halt
