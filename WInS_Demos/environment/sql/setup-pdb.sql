a2010c126321ee27672e3e86ae43133ae4613e4b
Jeff West <jeffrey.west@oracle.com>
2013-07-08 19:15:27 -0700

    added ACTIVE table for OPS

--- WInS_Demos/environment/sql/setup-pdb.sql
+++ WInS_Demos/environment/sql/setup-pdb.sql
@@ -29,5 +29,19 @@ CREATE TABLE ACTIVE (
 
 commit;
 
+
+connect ops_user / ops_user;
+
+CREATE TABLE ACTIVE (
+  SERVER VARCHAR2(150) NOT NULL,
+  INSTANCE VARCHAR2(100) NOT NULL,
+  DOMAINNAME VARCHAR2(50) NOT NULL,
+  CLUSTERNAME VARCHAR2(50) NOT NULL,
+  TIMEOUT DATE,
+  PRIMARY KEY (SERVER, DOMAINNAME, CLUSTERNAME)
+);
+
+commit;
+
 execute dbms_service.create_service('PDBORCL','PDBORCL');
 execute dbms_service.start_service('PDBORCL');
