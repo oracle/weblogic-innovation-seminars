b0b32768a350fe002ced199adb1be6d3510f6760
Jeff West <jeffrey.west@oracle.com>
2013-07-17 09:21:00 -0700

    updated to need to be run as root.

--- WInS_Demos/control/bin/prepareForExport.sh
+++ WInS_Demos/control/bin/prepareForExport.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #rm -Rf ~/.mozilla
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 . /u01/content/weblogic-innovation-seminars/WInS_Demos/control/bin/winsEnv.sh
 
 for param in $*
@@ -24,23 +29,23 @@ zerofree()
 SUCCESS="TRUE"
 
 
-sqlplus 'sys/welcome1 as sysdba' @/u01/content/weblogic-innovation-seminars/WInS_Demos/environment/sql/truncate.sql
+su oracle -c "sqlplus 'sys/welcome1 as sysdba' @/u01/content/weblogic-innovation-seminars/WInS_Demos/environment/sql/truncate.sql"
 
 killWebLogic.sh
 
-sudo rm -Rf $DOMAINS
-sudo rm -Rf /tmp/*
-sudo rm -Rf /var/cache/yum/*
-sudo rm -Rf /u01/content/oracle-parcel-service/ops-weblogic/Oracle
-sudo rm -Rf /home/oracle/Downloads/*
-sudo rm -Rf /home/oracle/.m2/repository/com/oracle/weblogic/demo
-sudo rm -Rf /home/oracle/.m2/repository/com/oracle/demo
+rm -Rf $DOMAINS
+rm -Rf /tmp/*
+rm -Rf /var/cache/yum/*
+rm -Rf /u01/content/oracle-parcel-service/ops-weblogic/Oracle
+rm -Rf /home/oracle/Downloads/*
+rm -Rf /home/oracle/.m2/repository/com/oracle/weblogic/demo
+rm -Rf /home/oracle/.m2/repository/com/oracle/demo
 
 cd /u01/content/weblogic-innovation-seminars/WInS_Demos
-mvn clean
+su oracle -c "mvn clean"
 
 cd /u01/content/oracle-parcel-service
-mvn clean
+su oracle -c "mvn clean"
 
 find /u01/content/weblogic-innovation-seminars -name "*.sh" -exec chmod +rx {} \;
 
@@ -52,7 +57,7 @@ fi
 if [ -h /u01/content/weblogic-innovation-seminars ]; then
   rm /u01/content/weblogic-innovation-seminars
   cd /u01/content
-  git clone http://github.com/oracle-weblogic/weblogic-innovation-seminars.git
+  su oracle -c "git clone http://github.com/oracle-weblogic/weblogic-innovation-seminars.git"
 
   if [ "$?" != "0" ]; then
     SUCCESS="FALSE"
@@ -63,19 +68,19 @@ fi
 if [ -h /u01/content/oracle-parcel-service ]; then
   rm /u01/content/oracle-parcel-service
   cd /u01/content
-  git clone http://github.com/jeffreyawest/oracle-parcel-service.git
+  su oracle -c "git clone http://github.com/jeffreyawest/oracle-parcel-service.git"
 
   if [ "$?" != "0" ]; then
     SUCCESS="FALSE"
   fi
 fi
 
-/u01/content/weblogic-innovation-seminars/WInS_Demos/control/bin/updateDemos.sh
+su oracle -c "/u01/content/weblogic-innovation-seminars/WInS_Demos/control/bin/updateDemos.sh"
 if [ "$?" != "0" ]; then
   SUCCESS="FALSE"
 fi
 
-/u01/content/weblogic-innovation-seminars/WInS_Demos/control/bin/updateOPS.sh
+su oracle -c "/u01/content/weblogic-innovation-seminars/WInS_Demos/control/bin/updateOPS.sh"
 if [ "$?" != "0" ]; then
   SUCCESS="FALSE"
 fi
