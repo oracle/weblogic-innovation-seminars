67c4582b8c3edca281868f1124127529af0118d5
Jeff West <jeffrey.west@oracle.com>
2013-07-24 12:26:48 -0700

    separated filename and directory

--- WInS_Demos/control/vbox/zipVM.cmd
+++ WInS_Demos/control/vbox/zipVM.cmd
@@ -2,27 +2,25 @@
 
 set overall_start_time=%time:~0,2%%time:~3,2%
 set dt=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%
-echo date_file=%dt%
 
-set OVA_FILE=%1
-set ZIP_OUTPUT_DIR=%2
+set DIR_NAME=%1
+set OVA_FILENAME=%2
+set ZIP_OUTPUT_DIR=%3
 
-rem  pause
-
-echo Zipping OVA=[%OVA_FILE%] into output dir=[%ZIP_OUTPUT_DIR%]
+echo Zipping OVA=[%OVA_FILENAME%] into output dir=[%ZIP_OUTPUT_DIR%]
 
 set ZIP_CLI_CMD=c:\data\7-zip\7z.exe
 set ZIP_METHOD=zip
 SET ZIP_COMPRESSION_LEVEL=2
 SET ZIP_MAX_SIZE=1g
-SET ZIP_DEST_FILE=%ZIP_OUTPUT_DIR%\%OVA_FILE%.zip
+SET ZIP_DEST_FILE=%ZIP_OUTPUT_DIR%\%OVA_FILENAME%.zip
 
 set zip_start_time=%time:~0,2%%time:~3,2%
 echo zip start time=%zip_start_time%
 
 set ZIP_CLI_OPTS=a -tzip -mx=1 -md=512m -mmt=4 -mfb=258 -mpass=2 -v%ZIP_MAX_SIZE%
 
-set CMDLINE=%ZIP_CLI_CMD% %ZIP_CLI_OPTS% %ZIP_DEST_FILE% %OVA_FILE% %OVA_FILE%.md5
+set CMDLINE=%ZIP_CLI_CMD% %ZIP_CLI_OPTS% %ZIP_DEST_FILE% %DIR_NAME%\%OVA_FILENAME% %DIR_NAME%\%OVA_FILENAME%.md5
 echo %CMDLINE%
 
 %CMDLINE%
