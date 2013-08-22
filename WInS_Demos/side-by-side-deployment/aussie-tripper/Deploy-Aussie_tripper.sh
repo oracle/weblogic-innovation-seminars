cd3ef8158da51e4e6e1d533b3d21ff44b96407c5
ankitshani <ankit.x.pandey@oracle.com>
2013-08-22 18:24:20 +0530

    ^M removed from pom.xml and all sh files

--- WInS_Demos/side-by-side-deployment/aussie-tripper/Deploy-Aussie_tripper.sh
+++ WInS_Demos/side-by-side-deployment/aussie-tripper/Deploy-Aussie_tripper.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-. "${WL_HOME}/server/bin/setWLSEnv.sh
+
+. ${WL_HOME}/server/bin/setWLSEnv.sh
+
+
 
 echo CLASSPATH=${CLASSPATH}
 
+
+
 export JVM_ARGS="-Dweblogic.security.TrustKeyStore=DemoTrust"
 
+
+
 ${JAVA_HOME}/bin/java ${JVM_ARGS} weblogic.Deployer -adminurl https://wins-vbox.localdomain:7200 -user weblogic -password welcome1 -name aussie-tripper-v1 -deploy -remote -upload aussie-tripper-v1.ear
+
--- WInS_Demos/side-by-side-deployment/aussie-tripper/Promote-Aussie_tripper_v2.sh
+++ WInS_Demos/side-by-side-deployment/aussie-tripper/Promote-Aussie_tripper_v2.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 
-. "${WL_HOME}/server/bin/setWLSEnv.sh
+. ${WL_HOME}/server/bin/setWLSEnv.sh
 
 echo CLASSPATH=${CLASSPATH}
 
 export JVM_ARGS="-Dweblogic.security.TrustKeyStore=DemoTrust"
 
 ${JAVA_HOME}/bin/java ${JVM_ARGS} weblogic.Deployer -adminurl https://wins-vbox.localdomain:7200 -user weblogic -password welcome1 -start -name aussie-tripper-v1 -appversion v2
+
