0cd31e8613eb9d1b9f4b81db6e4f7c840abc5481
nagypeter <nagypeter76@gmail.com>
2013-11-21 16:13:41 +0100

    start and stop the second pluggable database for pdb module.

---
+++ WInS_Demos/control/bin/startPDB2Database.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# chkconfig: 345 98 10
+# description: Oracle auto start-stop script.
+#
+# Set ORA_OWNER to the user id of the owner of the
+# Oracle database software.
+
+export TMP=/tmp
+export TMPDIR=$TMP
+export ORACLE_HOSTNAME=wins-vbox
+export ORACLE_UNQNAME=orcl
+export ORACLE_SID=orcl
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=$ORACLE_BASE/product/12.1/database
+export PATH=/usr/sbin:$ORACLE_HOME/bin:$PATH
+export ORACLE_OWNER=oracle
+
+$ORACLE_HOME/bin/sqlplus 'sys/welcome1 as sysdba' <<EOF
+
+alter pluggable database pdb2 open;
+
+exit
+EOF
+
+
