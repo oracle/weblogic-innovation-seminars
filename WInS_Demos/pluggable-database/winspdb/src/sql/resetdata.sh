2a25ae750c68614eb3216d513ae956c9a7dfae8e
wins-user <wins@wins-vbox.localdomain>
2015-02-12 06:06:57 -0800

    pluggable database module migration

--- WInS_Demos/pluggable-database/winspdb/src/sql/resetdata.sh
+++ WInS_Demos/pluggable-database/winspdb/src/sql/resetdata.sh
@@ -5,15 +5,7 @@
 # Set ORA_OWNER to the user id of the owner of the
 # Oracle database software.
 
-export TMP=/tmp
-export TMPDIR=$TMP
-export ORACLE_HOSTNAME=wins-vbox
-export ORACLE_UNQNAME=orcl
-export ORACLE_SID=orcl
-export ORACLE_BASE=/u01/app/oracle
-export ORACLE_HOME=$ORACLE_BASE/product/12.1/database
-export PATH=/usr/sbin:$ORACLE_HOME/bin:$PATH
-export ORACLE_OWNER=oracle
+. /u01/content/weblogic-innovation-seminars/WInS_Demos/control/bin/winsEnv.sh
 
 $ORACLE_HOME/bin/sqlplus 'c##cdbuser/welcome1@orcl' <<EOF
 
