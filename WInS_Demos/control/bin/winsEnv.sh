550461c62fa3efbbfba7d2ff3fd59f055608882a
Ankit Pandey <ankit.x.pandey@oracle.com>
2015-02-13 18:09:07 +0530

    Update winsEnv.sh

--- WInS_Demos/control/bin/winsEnv.sh
+++ WInS_Demos/control/bin/winsEnv.sh
@@ -33,7 +33,16 @@ export JAVA_HOME="/usr/java/latest"
 export PATH="${PATH}:${JAVA_HOME}/bin"
 
 export PATH="${PATH}:${DEMOS_HOME}/control/bin"
-
+export M2_HOME="/u01/middleware/oracle_common/modules/org.apache.maven_3.0.5"
+export MAVEN_HOME="${M2_HOME}"
+export MAVEN_OPTS="-Xmx2048m -Xms512m -Dweblogic.security.SSL.ignoreHostnameVerification=true -Dweblogic.security.TrustKeyStore=DemoTrust -Dweblogic.nodemanager.sslHostNameVerificationEnabled=false"
+export PATH="${PATH}:${M2_HOME}/bin"
+export MW_HOME="${SW_BASE}/middleware"
+export WL_HOME="${MW_HOME}/wlserver"
+export PATH="${PATH}:${WL_HOME}/common/bin"
+export PATH="${PATH}:${DEMOS_HOME}/control/bin"
+export ANT_HOME="${MW_HOME}/oracle_common/modules/org.apache.ant_1.9.2"
+export PATH=$ANT_HOME/bin:$PATH
 ###################################################################
 
 export BASICOUT="${DOMAINS}/weblogic-basic-domain/servers/AdminServer/logs/AdminServer.out"
