42e511ed5ff2fde0555d7bf817d455a9669765ae
unknown <ankitpan@ANKITPAN-LAP.oradev.oraclecorp.com>
2016-02-15 09:45:36 +0530

    nmStart properties added

--- WInS_Demos/CA-Workshop/Coherence.Federated.Cache/setup/startServers.py
+++ WInS_Demos/CA-Workshop/Coherence.Federated.Cache/setup/startServers.py
@@ -1,12 +1,14 @@
 #Connectiong to NodeManager in A_site_domain to Start AdminServer,cache_server1,webapp_server1
 nmConnect('weblogic','welcome1',domainName='A_site_domain',port='5556',host='localhost')
-nmStart('cache_server1')
-nmStart('webapp_server1')
+prps = makePropertiesObject("AdminURL=http://localhost:7001;Username=weblogic;Password=welcome1")
+nmStart('cache_server1',props=prps)
+nmStart('webapp_server1',props=prps)
 nmDisconnect()
 
 #Connectiong to NodeManager in B_site_domain to Start AdminServer,cache_server1,webapp_server1
 nmConnect('weblogic','welcome1',domainName='B_site_domain',port='5559',host='localhost')
-nmStart('cache_server1')
-nmStart('webapp_server1')
+prps = makePropertiesObject("AdminURL=http://localhost:8001;Username=weblogic;Password=welcome1")
+nmStart('cache_server1',props=prps)
+nmStart('webapp_server1',props=prps)
 nmDisconnect()
 
