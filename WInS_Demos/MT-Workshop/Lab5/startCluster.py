2d9cc2b83c014f9c350267471fbdf01ba0803268
unknown <ankitpan@ANKITPAN-LAP.oradev.oraclecorp.com>
2016-02-25 17:24:20 +0530

    Lab 5 HeapApp installed issue fixed

--- WInS_Demos/MT-Workshop/Lab5/startCluster.py
+++ WInS_Demos/MT-Workshop/Lab5/startCluster.py
@@ -6,8 +6,8 @@ start('app-cluster','Cluster')
 activate()
 
 print "************************ Deployng HeapApp in dp2 partition ****************************"
-startEdit()
+
 deploy(appName='heapApp', partition='dp2', resourceGroup='app2RG', path='/u01/content/weblogic-innovation-seminars/WInS_Demos/MT-Workshop/Lab5/heapApp.war')
-activate()
+
 
 disconnect()
