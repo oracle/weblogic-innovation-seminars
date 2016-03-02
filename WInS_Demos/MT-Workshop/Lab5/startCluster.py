a9bd444d8cecf827b71c208abbc8aad690ff324d
unknown <ankitpan@ANKITPAN-LAP.oradev.oraclecorp.com>
2016-03-02 10:00:01 +0530

    Deploying HeapApp before starting cluster

--- WInS_Demos/MT-Workshop/Lab5/startCluster.py
+++ WInS_Demos/MT-Workshop/Lab5/startCluster.py
@@ -5,9 +5,7 @@ startEdit()
 start('app-cluster','Cluster')
 activate()
 
-print "************************ Deployng HeapApp in dp2 partition ****************************"
 
-deploy(appName='heapApp', partition='dp2', resourceGroup='app2RG', path='/u01/content/weblogic-innovation-seminars/WInS_Demos/MT-Workshop/Lab5/heapApp.war')
 
 
 disconnect()
