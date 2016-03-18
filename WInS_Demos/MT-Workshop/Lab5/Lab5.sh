e7358a697db20328720fb8c34cb04a29f4748c34
unknown <ankitpan@ANKITPAN-LAP.oradev.oraclecorp.com>
2016-03-18 09:55:47 +0530

    Page 68 setDomainEnv.sh execution removed

--- WInS_Demos/MT-Workshop/Lab5/Lab5.sh
+++ WInS_Demos/MT-Workshop/Lab5/Lab5.sh
@@ -8,8 +8,8 @@ cd /u01/content/weblogic-innovation-seminars/WInS_Demos/MT-Workshop/Lab5
 cd /u01/content/weblogic-innovation-seminars/WInS_Demos/MT-Workshop/Lab5
 
 cp setDomainEnv.sh /u01/wins/wls1221/user_projects/domains/base_domain/bin/
-cd /u01/wins/wls1221/user_projects/domains/base_domain/bin
-. ./setDomainEnv.sh
+#cd /u01/wins/wls1221/user_projects/domains/base_domain/bin
+#. ./setDomainEnv.sh
 
 echo "Starting Server in app-cluster with RCM enabled"
 cd /u01/content/weblogic-innovation-seminars/WInS_Demos/MT-Workshop/Lab5
