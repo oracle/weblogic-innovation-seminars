42b18affba7b35d2148d555210f2476f957248e4
unknown <ankitpan@ANKITPAN-LAP.oradev.oraclecorp.com>
2016-06-24 17:39:39 +0530

    Lift And Shift Version 1

--- WInS_Demos/MT-Workshop/Lab8/prepare_env.sh
+++ WInS_Demos/MT-Workshop/Lab8/prepare_env.sh
@@ -11,7 +11,7 @@ cd /u01/content/weblogic-innovation-seminars/WInS_Demos/MT-Workshop/Lab8
 export JAVA_HOME=/usr/java/jdk1.7.0_79/
 export MW_HOME=/u01/wins/wls1036/
 
-echo “***************** STARTING ADMIN SERVER IN WEBLOGIC 10.3.6 DOMAIN (DOMAIN1036) ******************”
+echo “***************** STARTING ADMIN SERVER IN WEBLOGIC 10.3.6 DOMAIN DOMAIN1036 ******************”
 cd /u01/wins/wls1036/user_projects/domains/Domain1036/bin
 nohup ./startWebLogic.sh &>adminserver.log </dev/null  &
 sleep 90 
@@ -19,9 +19,8 @@ sleep 90
 export JAVA_HOME=/usr/java/latest/
 export MW_HOME=/u01/wins/wls1221/
 
-echo “***************** STARTING ADMIN SERVER IN WEBLOGIC 12.2.1 DOMAIN (DOMAIN1221) ******************”
+echo “***************** STARTING ADMIN SERVER IN WEBLOGIC 12.2.1 DOMAIN DOMAIN1221 ******************”
 cd /u01/wins/wls1221/user_projects/domains/Domain1221/bin
 nohup ./startWebLogic.sh &>adminserver.log </dev/null  &
 sleep 90
-
 
