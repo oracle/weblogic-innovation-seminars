550086605fdd51ff460e62601e2faa59f3b4ff41
MaciejGruszka <maciej.gruszka@oracle.com>
2016-07-27 10:58:52 -0700

    Update createSSHKeys.sh

--- WInS_Demos/MT-Workshop/createSSHKeys.sh
+++ WInS_Demos/MT-Workshop/createSSHKeys.sh
@@ -3,3 +3,6 @@
 ssh-keygen -t rsa -N "" -b "2048" -C "winsmt" -f /u01/content/weblogic-innovation-seminars/WInS_Demos/MT-Workshop/winsmt
 cd /u01/content/weblogic-innovation-seminars/WInS_Demos/MT-Workshop/
 chmod 600 winsmt
+cp /u01/content/weblogic-innovation-seminars/WInS_Demos/MT-Workshop/winsmt /u01/content/weblogic-innovation-seminars/cloud.demos/pk.openssh
+chmod 600 /u01/content/weblogic-innovation-seminars/cloud.demos/pk.openssh
+
