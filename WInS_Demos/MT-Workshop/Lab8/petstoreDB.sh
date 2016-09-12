6cc37c0ee7e22f90b1c5647ba5400be7311eee9a
pnagy <pnagy@email.com>
2016-09-12 14:43:22 +0200

    Lift and shift Lab8: drop user before create

--- WInS_Demos/MT-Workshop/Lab8/petstoreDB.sh
+++ WInS_Demos/MT-Workshop/Lab8/petstoreDB.sh
@@ -2,12 +2,15 @@
 cd /u01/content/weblogic-innovation-seminars/WInS_Demos/MT-Workshop/Lab8
 
 sqlplus system/welcome1@localhost:1521/pdborcl <<EOF
+ drop user petstore cascade;
+
  create user petstore identified by petstore;
  grant DBA to petstore;
  exit;
 EOF
 
 sqlplus petstore/petstore@localhost:1521/pdborcl <<EOF
- @petstore.sql
+ @DBCS/petstore.sql
  exit;
 EOF
+
