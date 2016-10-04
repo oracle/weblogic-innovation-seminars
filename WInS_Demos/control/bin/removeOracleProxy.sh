9af1fa1c4df432edb1dd871ea6191a79a0341e09
pnagy <pnagy@email.com>
2016-10-04 06:21:50 +0200

    yum proxy configurations

--- WInS_Demos/control/bin/removeOracleProxy.sh
+++ WInS_Demos/control/bin/removeOracleProxy.sh
@@ -2,8 +2,23 @@
 
 . /u01/content/weblogic-innovation-seminars/WInS_Demos/control/bin/winsEnv.sh
 
+ORACLE_HTTP_PROXY="http:\/\/www-proxy.us.oracle.com:80"
+
 echo "Remove Maven proxy settings"
 rm -f ~/.m2/settings.xml
+
+#=========================================================
+YUMCONF_FILE=/etc/yum.conf
+
+grepresult=$(grep -c "proxy=$ORACLE_HTTP_PROXY" $YUMCONF_FILE -s)
+
+if [ $grepresult == 1 ]
+then
+    # yum configured for proxy, need to delete
+    sudo sed -i "/proxy=$ORACLE_HTTP_PROXY/ d" $YUMCONF_FILE
+    echo "proxy has been removed from yum configuration"
+fi
+#=========================================================
 
 PROXY_SCRIPT="/home/oracle/setProxy.sh"
 PROXY_MESSAGE="Proxy NOT SET for Oracle Network!!!"
--- WInS_Demos/control/bin/setOracleProxy.sh
+++ WInS_Demos/control/bin/setOracleProxy.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
+ORACLE_HTTP_PROXY="http:\/\/www-proxy.us.oracle.com:80"
+
 echo "Set Maven proxy settings"
 cp /u01/content/weblogic-innovation-seminars/WInS_Demos/control/maven/settings.xml ~/.m2/
 
+#=========================================================
+YUMCONF_FILE=/etc/yum.conf
+
+grepresult=$(grep -c "proxy=$ORACLE_HTTP_PROXY" $YUMCONF_FILE -s)
+
+if [ $grepresult == 1 ]
+then
+    # yum configured for proxy, need to delete
+    echo "yum is already configured for proxy."
+else
+    # yum not configured for proxy, need to add
+    sudo sed -i '$ a\'"proxy=$ORACLE_HTTP_PROXY"'' $YUMCONF_FILE
+    echo "yum now has been configured for proxy."
+fi
+#=========================================================
+
 PROXY_SCRIPT="/home/oracle/setProxy.sh"
-ORACLE_HTTP_PROXY="http://www-proxy.us.oracle.com:80"
 PROXY_MESSAGE="Proxy Configured for Oracle Network!!!"
 
 echo "#!/bin/sh" > ${PROXY_SCRIPT}
